1395a9814baabbab10cca177cf0a20522cbe41ee
CpW-666 <masakaer20@gmail.com>
2025-04-15 11:18:18 +0700

    Add files via upload

---
+++ week_6/Palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+// Kelas Main (program utama)
+public class Palindrome {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("=========== Palindrome ===========");
+      System.out.println("===== Ciptanur Wahid Putera =====");
+      String again = "y";
+
+      while(again.equalsIgnoreCase("y")){
+        System.out.print("Masukkan kata: ");
+        String input = scan.nextLine();
+        TextGame game1 = new TextGame();
+        TextGame game2 = new FancyTextGame();
+        game1.displayResult(input);
+        game2.displayResult(input);
+        System.out.print("Apakah Anda ingin mencoba lagi? (y/n): ");
+        again = scan.nextLine(); 
+        System.out.println();
+      }
+
+    }
+}
